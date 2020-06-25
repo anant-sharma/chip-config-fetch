94c7483c03bef80bab358fabf6e7e790b6f9ec9d
Anant Sharma <anantatnet@gmail.com>
2020-06-25 16:03:28 +0530

    Added Deployment Logs

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,9 @@
 import * as core from '@actions/core'
-import axios from 'axios'
+import axios, {AxiosInstance, AxiosResponse} from 'axios'
+
+interface INull {
+  [key: string]: string | Object
+}
 
 interface IConfig {
   REPO_NAME: string
@@ -95,79 +99,122 @@ async function deployService(config: IConfig): Promise<void> {
 
   try {
     // Pull New Image
-    await cAxios({
-      method: 'post',
-      url: '/images/create',
-      params: {
-        fromImage: `${config.DOCKER_USER}/${config.DOCKER_IMAGE_NAME}`,
-        tag: config.TAG
-      },
-      data: {}
-    })
+    await pullImage(cAxios, config)
     core.debug('Image Pull Complete')
 
     // Remove Old Image
-    await cAxios({
-      method: 'delete',
-      url: `/containers/${config.CLUSTER_CONTAINER_NAME}`,
-      params: {
-        v: true,
-        force: true
-      },
-      data: {}
-    }).catch(() => {})
+    await removeOldImage(cAxios, config).catch(() => {})
     core.debug('Remove Old Complete')
 
     // Create New Container
-    await cAxios({
-      method: 'post',
-      url: `/containers/create`,
-      params: {
-        name: config.CLUSTER_CONTAINER_NAME
-      },
-      data: {
-        Image: `${config.DOCKER_USER}/${config.DOCKER_IMAGE_NAME}:${config.TAG}`,
-        ExposedPorts: config.PORTS.reduce(
-          (result: {[key: string]: Object}, port) => {
-            result[`${port.container}/tcp`] = {}
-            return result
-          },
-          {}
-        ),
-        HostConfig: {
-          PortBindings: config.PORTS.reduce(
-            (result: {[key: string]: Object}, port) => {
-              result[`${port.container}/tcp`] = [
-                {
-                  HostPort: port.host
-                }
-              ]
-              return result
-            },
-            {}
-          ),
-          PublishAllPorts: true,
-          AutoRemove: true,
-          NetworkMode: config.NETWORK_MODE || 'bridge'
-        },
-        name: config.CLUSTER_CONTAINER_NAME
-      }
-    })
-    core.debug('Create New Complete')
+    await createContainer(cAxios, config)
+    core.debug('Create New Container Complete')
 
     // Start New Container
-    await cAxios({
-      method: 'post',
-      url: `/containers/${config.CLUSTER_CONTAINER_NAME}/start`,
-      params: {},
-      data: {}
-    })
-    core.debug('Start New Complete')
+    await startContainer(cAxios, config)
+    core.debug('Start New Container Complete')
 
     return
   } catch (e) {
     return Promise.reject(e)
   }
+}
+
+async function pullImage(
+  cAxios: AxiosInstance,
+  config: IConfig
+): Promise<AxiosResponse<INull>> {
+  const fromImage = `${config.DOCKER_USER}/${config.DOCKER_IMAGE_NAME}`
+  const tag = config.TAG
+
+  core.debug(`Pulling Image ${fromImage}:${tag}`)
+
+  return cAxios({
+    method: 'post',
+    url: '/images/create',
+    params: {
+      fromImage,
+      tag
+    }
+  })
+}
+
+async function removeOldImage(
+  cAxios: AxiosInstance,
+  config: IConfig
+): Promise<AxiosResponse<INull>> {
+  core.debug(`Removing Image ${config.CLUSTER_CONTAINER_NAME}`)
+
+  return cAxios({
+    method: 'delete',
+    url: `/containers/${config.CLUSTER_CONTAINER_NAME}`,
+    params: {
+      v: true,
+      force: true
+    },
+    data: {}
+  })
+}
+
+async function createContainer(
+  cAxios: AxiosInstance,
+  config: IConfig
+): Promise<AxiosResponse<INull>> {
+  const params = {
+    name: config.CLUSTER_CONTAINER_NAME
+  }
+
+  const data = {
+    Image: `${config.DOCKER_USER}/${config.DOCKER_IMAGE_NAME}:${config.TAG}`,
+    ExposedPorts: config.PORTS.reduce(
+      (result: {[key: string]: Object}, port) => {
+        result[`${port.container}/tcp`] = {}
+        return result
+      },
+      {}
+    ),
+    HostConfig: {
+      PortBindings: config.PORTS.reduce(
+        (result: {[key: string]: Object}, port) => {
+          result[`${port.container}/tcp`] = [
+            {
+              HostPort: port.host
+            }
+          ]
+          return result
+        },
+        {}
+      ),
+      PublishAllPorts: true,
+      AutoRemove: true,
+      NetworkMode: config.NETWORK_MODE || 'bridge'
+    },
+    name: config.CLUSTER_CONTAINER_NAME
+  }
+
+  core.debug(`Creating container with params ${JSON.stringify(params)}`)
+  core.debug(`Creating container with data ${JSON.stringify(data)}`)
+
+  return cAxios({
+    method: 'post',
+    url: `/containers/create`,
+    params,
+    data
+  })
+}
+
+async function startContainer(
+  cAxios: AxiosInstance,
+  config: IConfig
+): Promise<AxiosResponse<INull>> {
+  core.debug(`Starting Container Image ${config.CLUSTER_CONTAINER_NAME}`)
+
+  return cAxios({
+    method: 'post',
+    url: `/containers/${config.CLUSTER_CONTAINER_NAME}/start`,
+    params: {},
+    data: {}
+  })
 }
 
 run()
